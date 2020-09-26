7731f359b6717cdc172cd9d182ff32b3584a30f8
h3xg <h3xg@qq.com>
2020-09-26 13:26:12 +0800

    optimized the ./_MoonPage.css

--- js/jsForMoonPage.js
+++ js/jsForMoonPage.js
@@ -5,6 +5,7 @@
     var redM = document.querySelector(".redMoon");
     var per = (100-e.clientY/window.innerHeight*100); //为符合逻辑，100去减
     // redM.style.clipPath = `inset(0 ${per}% 0 0)`; //且注意是` 非 ’//See SVG element reference<clipPath>
-    redM.style.clipPath = `polygon(${per+10}% 0, 100% 0, 100% 100%, ${per}% 100%)`; //且注意是` 非 ’//See SVG element reference<clipPath>
+    if(per<=10.5) per = 10.5;  //仅美化意义
+    redM.style.clipPath = `polygon(${per}% 0, 100% 0, 100% 100%, ${per-10}% 100%)`; //且注意是` 非 ’//See SVG element reference<clipPath>
   });
 // }
